e2edc7cc66860eab36509d86b2bed78d6d82ef52
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2023-10-24 01:55:48 +0200

    feat: added Hero component, styles, hero images, updated Container, homepage and layout

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,24 +1,23 @@
 import { NextPage } from "next";
+import HeroComponent from "@/components/HeroComponent/HeroComponent";
+import { getAllTranslations, getTranslation } from "@/utils/dictionary";
 import { Locales } from "@/types";
 
-interface IProps {
+interface IHomePageProps {
   params: {
     lang: Locales;
   };
 }
 
-const HomePage: NextPage<IProps> = () => (
-  <div style={{ display: "flex ", flexDirection: "column" }}>
-    <div>Hero Section</div>
-    <div>War Need Help Section</div>
-    <div>Що таке Місто Добра?</div>
-    <div>
-      Ми звертаємося до всіх небайдужих – простягніть руку допомоги Україні у
-      цей страшний час, простягніть руку допомоги українським дітям!
-    </div>
-    <div>Чому Місто Добра потрібне?</div>
-    <div>і так далі</div>
-  </div>
-);
+const HomePage: NextPage<IHomePageProps> = async ({ params: { lang } }) => {
+  const language = await getAllTranslations(lang);
+  const t = getTranslation(language);
+
+  return (
+    <>
+      <HeroComponent data={t("hero")} />
+    </>
+  );
+};
 
 export default HomePage;
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,10 +1,17 @@
 import React, { ReactNode } from "react";
 import styles from "./Container.module.css";
+import clsx from "clsx";
 
 interface ContainerProps {
   children: ReactNode;
+  className?: string;
 }
 
-export const Container: React.FC<ContainerProps> = ({ children }) => (
-  <div className={styles.container}>{children}</div>
+export const Container: React.FC<ContainerProps> = ({
+  children,
+  className,
+}) => (
+  <div className={clsx(styles.container, className && className)}>
+    {children}
+  </div>
 );
