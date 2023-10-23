85593a85c862c5f9505ae00e6d03708e498c9b1d
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-10-23 15:12:48 +0300

    feat/add howToHelp component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { Footer } from "./footer";
 export { Nav } from "./nav";
 export { Container } from "./Container/Container";
 export { Button } from "./Button/Button";
+export { HowToHelp } from "./HowToHelp/HowToHelp";
