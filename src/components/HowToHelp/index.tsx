ff3f3283c508979cacabdd21ef75b89ee37a1307
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-10-25 14:58:12 +0300

    fix prettier

--- src/components/HowToHelp/index.tsx
+++ src/components/HowToHelp/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 import { FC } from "react";
-import Image from "next/image";
 import Accordion from "@/components/HowToHelp/Accordion";
 import styles from "./HowToHelp.module.css";
 
@@ -8,15 +7,11 @@ interface IHowToHelpProps {
   data: {
     title: string;
     paragraph: string;
-    bankTransfer: string;
-    onlinePayment: string;
-    monobank: string;
-    payPal: string;
   };
 }
 
 export const HowToHelp: FC<IHowToHelpProps> = ({
-  data: { title, paragraph, bankTransfer, onlinePayment, monobank, payPal },
+  data: { title, paragraph },
 }) => (
   <section className={styles.howToHelp}>
     <div>
