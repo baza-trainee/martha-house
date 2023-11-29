aad9a60b2927a1814e50a21a9f372718e7f0e5d7
Matviy <antipin.m9971@gmail.com>
2023-11-29 18:15:14 +0200

    add requests on strapi to news page

--- src/app/(landing)/[lang]/blog/page.tsx
+++ src/app/(landing)/[lang]/blog/page.tsx
@@ -1,12 +1,24 @@
 import { NextPage } from "next";
 import { Locales } from "@/types";
 import { getTranslation, getAllTranslations } from "@/utils/dictionary";
+// import config from "@/config";
 
 interface IProps {
   params: {
     lang: Locales;
   };
 }
+
+// const fetchBlogs = async () => {
+//   const reqOption = {
+//     headers: {
+//       Authorization: `Baerer ${process.env.API_KEY}`,
+//     },
+//   };
+
+//   const request = await fetch(`${config.api}/api/blogs?populate=*`, reqOption);
+//   const response = await request.json();
+// };
 
 const BlogPage: NextPage<IProps> = async ({ params: { lang } }) => {
   const language = await getAllTranslations(lang);
