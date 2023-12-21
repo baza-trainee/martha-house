a50a52a3c84c1d2fb9cd7143c354f66a74ff88d6
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-12-22 00:16:11 +0200

    feat: start create card to blog

--- src/app/(landing)/[lang]/blog/components/ArticleItem/ArticleItem.tsx
+++ src/app/(landing)/[lang]/blog/components/ArticleItem/ArticleItem.tsx
@@ -2,6 +2,7 @@ import { ArticleData } from "@/types/ArtcilesData";
 import styles from "./ArticleItem.module.css";
 
 const ArticleItem = ({ data }: ArticleData) => {
+  // eslint-disable-next-line no-console
   console.log(data);
   return (
     <div className={styles.article}>
