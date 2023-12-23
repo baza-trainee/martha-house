463f79ebcb2d13ee2149af76c77b4164354febf2
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2023-12-23 03:08:41 +0100

    created Heading component

--- src/app/(landing)/[lang]/news/page.tsx
+++ src/app/(landing)/[lang]/news/page.tsx
@@ -1,23 +1,25 @@
 import { NextPage } from "next";
 import { Locales } from "@/types";
+import Heading from "./components/Heading/Heading";
+import Container from "@/components/Container";
 import { getTranslation, getAllTranslations } from "@/utils/dictionary";
 import config from "@/config";
 
-const fetchNews = async (params: string) => {
-  const reqOptions: object = {
-    headers: {
-      Authorization: `Bearer ${process.env.API_TOKEN}`,
-    },
-  };
+// const fetchNews = async (params: string) => {
+//   const reqOptions: object = {
+//     headers: {
+//       Authorization: `Bearer ${process.env.API_TOKEN}`,
+//     },
+//   };
 
-  const req = await fetch(
-    `${config.api}/api/news?populate=*&${params}`,
-    reqOptions,
-  );
-  const res = await req.json();
+//   const req = await fetch(
+//     `${config.api}/api/news?populate=*&${params}`,
+//     reqOptions,
+//   );
+//   const res = await req.json();
 
-  return res;
-};
+//   return res;
+// };
 
 interface IProps {
   params: {
@@ -33,16 +35,16 @@ const NewsPage: NextPage<IProps> = async ({ params: { lang } }) => {
   const language = await getAllTranslations(lang);
   const t = getTranslation(language);
 
-  const [futuredNews, news] = await Promise.all([
-    await fetchNews("filters[isFutured][$eq]=true"),
-    await fetchNews("filters[isFutured][$eq]=false"),
-  ]);
+  // const [futuredNews, news] = await Promise.all([
+  //   await fetchNews("filters[isFutured][$eq]=true"),
+  //   await fetchNews("filters[isFutured][$eq]=false"),
+  // ]);
 
-  console.log("futuredNews>>>>", futuredNews.data);
+  // console.log("futuredNews>>>>", futuredNews.data);
 
   return (
-    <main>
-      <ul>
+    <div>
+      {/* <ul>
         {futuredNews.data.map((item: any) => (
           <li key={item.id}>
             <h3>Category: {item.attributes.Category}</h3>
@@ -61,9 +63,12 @@ const NewsPage: NextPage<IProps> = async ({ params: { lang } }) => {
             <img src={item.attributes.Image.data[0].attributes.url} alt="" />
           </li>
         ))}
-      </ul>
-      <h1>{t("news.title")}</h1>
-    </main>
+      </ul> */}
+      <Container>
+        <Heading data={t("news")} />
+      </Container>
+      <div>NewsCard</div>
+    </div>
   );
 };
 
