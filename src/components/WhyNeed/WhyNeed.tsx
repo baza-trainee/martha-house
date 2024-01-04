0242bb61cedfd31957fadbb91f00172c272037f8
Kateryna Laina <boba4ok@gmail.com>
2024-01-04 13:57:57 +0200

    fix styling

--- src/components/WhyNeed/WhyNeed.tsx
+++ src/components/WhyNeed/WhyNeed.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
+import { CustomImage } from "@/components";
 import styles from "./WhyNeed.module.css";
-import Container from "../Container";
-import { CustomImage } from "../CustomImage";
 
 interface WhyneedProps {
   data: {
@@ -13,45 +12,43 @@ interface WhyneedProps {
 
 const WhyNeed: FC<WhyneedProps> = ({ data: { title, images, paragraphs } }) => (
   <section className={styles.whyneed}>
-    <Container>
-      <h2 className={styles.whyneedTitle}>{title}</h2>
-      <div className={styles.whyneedWrapper}>
-        <p className={`${styles.whyneedItem} ${styles.whyneedDescr}`}>
-          {paragraphs[0]}
-        </p>
-        <div className={`${styles.whyneedItem} ${styles.whyneedImgContainer}`}>
-          <CustomImage
-            height={189}
-            src={images[0]}
-            alt="children"
-            width={343}
-            className={styles.whyneedImg}
-          />
-        </div>
+    <h2 className={styles.whyneedTitle}>{title}</h2>
+    <div className={styles.whyneedWrapper}>
+      <p className={`${styles.whyneedItem} ${styles.whyneedDescr}`}>
+        {paragraphs[0]}
+      </p>
+      <div className={`${styles.whyneedItem} ${styles.whyneedImgContainer}`}>
+        <CustomImage
+          height={189}
+          src={images[1]}
+          alt="children"
+          width={343}
+          className={styles.whyneedImg}
+        />
       </div>
-      <div className={styles.whyneedWrapper}>
-        <p className={`${styles.whyneedItem} ${styles.whyneedDescr}`}>
-          {paragraphs[1]}
-        </p>
-        <p className={`${styles.whyneedItem} ${styles.whyneedDescr}`}>
-          {paragraphs[2]}
-        </p>
+    </div>
+    <div className={styles.whyneedWrapper}>
+      <p className={`${styles.whyneedItem} ${styles.whyneedDescr}`}>
+        {paragraphs[1]}
+      </p>
+      <p className={`${styles.whyneedItem} ${styles.whyneedDescr}`}>
+        {paragraphs[2]}
+      </p>
+    </div>
+    <div className={styles.whyneedWrapper}>
+      <p className={`${styles.whyneedItem} ${styles.whyneedDescr}`}>
+        {paragraphs[3]}
+      </p>
+      <div className={`${styles.whyneedItem} ${styles.whyneedImgContainer}`}>
+        <CustomImage
+          height={189}
+          src={images[0]}
+          alt="children"
+          width={343}
+          className={styles.whyneedImg}
+        />
       </div>
-      <div className={styles.whyneedWrapper}>
-        <p className={`${styles.whyneedItem} ${styles.whyneedDescr}`}>
-          {paragraphs[3]}
-        </p>
-        <div className={`${styles.whyneedItem} ${styles.whyneedImgContainer}`}>
-          <CustomImage
-            height={189}
-            src={images[1]}
-            alt="children"
-            width={343}
-            className={styles.whyneedImg}
-          />
-        </div>
-      </div>
-    </Container>
+    </div>
   </section>
 );
 
